0e947d7a1e36ee1ae6583ca966a67d9200a17161
Ben Civjan <bencivjan@gmail.com>
2023-11-06 11:26:28 -0600

    Stream video data in chunks with video that fits into 1 chunk

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,15 @@ use core::num;
 use errors::ServerError;
 use httparse;
 use std::{
-    fs::File, io::prelude::*, net::TcpListener, net::TcpStream, str, thread, time::Duration,
+    fs::File, fs::metadata, io::prelude::*, net::TcpListener, net::TcpStream, str, thread, time::Duration,
 };
+
+struct Chunk {
+    bytes: Vec<u8>,
+    content_start: u64,
+    content_end: u64,
+    content_length: u64
+}
 
 fn main() -> Result<(), ServerError> {
     let listener = TcpListener::bind("127.0.0.1:7878")
@@ -20,7 +27,7 @@ fn main() -> Result<(), ServerError> {
         match stream {
             Ok(s) => {
                 handle_connection(s).unwrap();
-            },
+            }
             Err(e) => {
                 eprintln!("Failed to create stream for TCP connection: {}", e);
             }
@@ -38,51 +45,82 @@ fn main() -> Result<(), ServerError> {
     Ok(())
 }
 
-fn read_video_section(file_path: &str, buffer: &mut Vec<u8>) -> Result<usize, ServerError> {
-    // let mut buffer = vec![0; BLOCK_SIZE];
-    let mut file = File::open(file_path).unwrap();
-    // let mut num_block = 0;
-    let mut total_bytes_read = 0;
-    loop {
-        let br = file.read(buffer.as_mut()).unwrap();
-        total_bytes_read += br;
+// fn read_video_section(file_path: &str, buffer: &mut Vec<u8>) -> Result<usize, ServerError> {
+//     // let mut buffer = vec![0; BLOCK_SIZE];
+//     let mut file = File::open(file_path).unwrap();
+//     // let mut num_block = 0;
+//     // let mut total_bytes_read = 0;
+//     loop {
+//     let br = file.read(buffer.as_mut()).unwrap();
+//     total_bytes_read += br;
 
-        if br == 0 {
-            break;
-        }
-    }
-    // println!("{}", num_block);
-    Ok(total_bytes_read)
-}
+//     if br == 0 {
+//         break;
+//     }
+//     }
+//     println!("{}", num_block);
+//     Ok(br)
+// }
 
 fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Error>> {
-    let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
+    let mut req_buffer = [0; 1024];
+    stream.read(&mut req_buffer).unwrap();
     let mut headers = [httparse::EMPTY_HEADER; 64];
     let mut req = httparse::Request::new(&mut headers);
 
-    let get = b"GET / HTTP/1.1\r\n";
-    // const get_movie = b"GET /movie.mp4 HTTP/1.1\r\n";
-    // const sleep = b"GET /sleep HTTP/1.1\r\n";
+    println!("{:?}", str::from_utf8(&req_buffer).unwrap());
+    req.parse(&req_buffer)?;
 
-    println!("{:?}", str::from_utf8(&buffer).unwrap());
-    req.parse(&buffer)?;
+    println!("{:?}", req);
 
-    const BLOCK_SIZE: usize = 1024 * 400; //376852115; // 383631
-    let mut file_buffer = vec![0; BLOCK_SIZE];
+    const BLOCK_SIZE: usize = 1024 * 400; //376852115;
+    let file_buffer = vec![0; BLOCK_SIZE];
     let file_path = "../movie.mp4"; // "../v_day_climb_carry.mp4";
-    let file_nbytes = read_video_section(file_path, &mut file_buffer).unwrap();
 
-    println!("Read {} bytes from {}", file_nbytes, file_path);
+    let mut file = File::open(file_path)?;
+    let content_length = metadata(file_path)?.len();
+
+    let mut chunk = Chunk {
+        bytes: file_buffer,
+        content_start: 0,
+        content_end: 0,
+        content_length: content_length,
+    };
+    
+    loop {
+        let br = file.read(chunk.bytes.as_mut()).unwrap() as u64;
+
+        // update chunk fields
+        chunk.content_start = chunk.content_end;
+        chunk.content_end = chunk.content_start + br - 1;
+    
+        if br == 0 {
+            break;
+        }
+        println!("Bytes read from file: {}", br);
+
+        // process the data
+        stream_data(&mut stream, &req_buffer, &mut chunk)?;
+    }
+    // let file_nbytes = read_video_section(file_path, &mut file_buffer).unwrap();
+
+    // println!("Read {} bytes from {}", file_nbytes, file_path);
 
     // TODO: Handle the partial parse case?
     // if req.parse(&buffer)?.is_partial() {
     // }
 
-    println!("{:?}", req.headers);
+    // println!("{:?}", req.headers);
 
-    let status_line = if buffer.starts_with(get) {
-        "HTTP/1.1 200 OK"
+    Ok(())
+}
+
+fn stream_data(stream: &mut TcpStream, request: &[u8], chunk: &mut Chunk) -> Result<(), Box<dyn std::error::Error>> {
+    let get = b"GET / HTTP/1.1\r\n";
+    let nbytes: usize = (chunk.content_end - chunk.content_start + 1).try_into().unwrap();
+
+    let status_line = if request.starts_with(get) {
+        "HTTP/1.1 206 Partial Content" // "HTTP/1.1 200 OK"
     } else {
         "HTTP/1.1 404 NOT FOUND"
     };
@@ -91,16 +129,25 @@ fn handle_connection(mut stream: TcpStream) -> Result<(), Box<dyn std::error::Er
     println!("{}", status_line);
 
     let response = format!(
-        "{}\r\nContent-Type: application/octet-stream\r\nAccept-Ranges: bytes\r\nAccess-Control-Allow-Origin: *\r\nContent-Length: {}\r\n\r\n",
+        "{}\r\n\
+        Accept-Ranges: bytes \r\n\
+        Content-Range: bytes {}-{}/{} \r\n\
+        Content-Type: application/octet-stream \r\n\
+        Content-Length: {} \r\n\
+        Access-Control-Allow-Origin: * \r\n\r\n",
         status_line,
-        file_nbytes,
+        chunk.content_start,
+        chunk.content_end,
+        chunk.content_length,
+        chunk.content_length
     );
 
     println!("{}", response);
+    println!("nbytes: {}", nbytes);
 
     stream.write_all(response.as_bytes()).unwrap();
-    let file_bytes_written = stream.write(&file_buffer).unwrap();
-    println!("{}", file_bytes_written);
+    let file_bytes_written = stream.write(&chunk.bytes[0..nbytes]).unwrap();
+    println!("bytes written to stream: {}", file_bytes_written);
     stream.flush().unwrap();
 
     Ok(())
